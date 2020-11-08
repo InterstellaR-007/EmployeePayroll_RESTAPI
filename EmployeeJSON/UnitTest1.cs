09361cfc5d9eb9a45853df174844425738f542e2
unknown <intrstelr.007@gmail.com>
2020-11-08 12:27:29 +0530

    [Anuj] Added Employees Retreival cur op

--- EmployeeJSON/UnitTest1.cs
+++ EmployeeJSON/UnitTest1.cs
@@ -7,10 +7,31 @@ using System.Security.Cryptography.X509Certificates;
 
 namespace EmployeeJSON
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public class Employee
     {
+        /// <summary>
+        /// Gets or sets the identifier.
+        /// </summary>
+        /// <value>
+        /// The identifier.
+        /// </value>
         public int id { get; set; }
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string name { get; set; }
+        /// <summary>
+        /// Gets or sets the salary.
+        /// </summary>
+        /// <value>
+        /// The salary.
+        /// </value>
         public string salary { get; set; }
 
     }
@@ -20,12 +41,16 @@ namespace EmployeeJSON
 
         RestClient client;
 
+        /// <summary>
+        /// Setups this instance.
+        /// </summary>
         [TestInitialize]
         public void Setup()
         {
-            client = new RestClient("http://localhost:4000");
+            client = new RestClient("http://localhost:3000");
         }
 
+        /// <summary>Called when [list return employee list].</summary>
         [TestMethod]
         public void OncallingList_ReturnEmployeeList()
         {
@@ -35,10 +60,11 @@ namespace EmployeeJSON
             List<Employee> dataresponse = JsonConvert.DeserializeObject<List<Employee>>(response.Content);
             Assert.AreEqual(4, dataresponse.Count);
 
-
-            
-            
         }
+        /// <summary>Gets all employees.</summary>
+        /// <returns>
+        ///   <br />
+        /// </returns>
         private IRestResponse getAllEmployees()
         {
             RestRequest request = new RestRequest("/employees", Method.GET);
